ccba1e99ba22e077c55f6a545895db62c394e7a6
Valentino Mettifogo <valentino.mettifogo@gmail.com>
2026-05-09 14:56:07 +0200

    new original icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import "./layout.css";
-	import favicon from "$lib/assets/favicon.svg";
+	import favicon from "$lib/assets/favicon.png";
 	import Navbar from "$lib/components/Navbar.svelte";
 
 	let { children, data } = $props();
