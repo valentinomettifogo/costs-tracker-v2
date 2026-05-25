86ccf277bc1cf3ad366d285cc5596702a49b7ab0
Valentino Mettifogo <valentino.mettifogo@gmail.com>
2026-05-25 22:17:29 +0200

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import "./layout.css";
 	import Navbar from "$lib/components/Navbar.svelte";
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 	let { children, data } = $props();
 </script>
